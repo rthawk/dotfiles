d885bb5db0cd19fbc973be48c2eed6fe184fe8cb
rthawk <rthawk@rtks.net>
2016-02-08 15:58:01 +0000

    fix create_symlink.sh. backup and remove old links

--- create_symlink.sh
+++ create_symlink.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+
+BACKUPDIR=$HOME/dotfiles.bk.$(date +"%Y-%m-%d.%H%M%S")
+mkdir $BACKUPDIR
+
 cd $(dirname $0)
+
+echo "home directory path: $HOME"
+echo "backup directory:  $BACKUPDIR"
+echo "dotfiles path:       $PWD \n"
+echo "installing dotfiles\n---"
+
 for dotfile in .?*; do
     case $dotfile in
         *.elc)
@@ -11,8 +21,16 @@ for dotfile in .?*; do
         .gitignore)
             continue;;
         *)
-            ln -snfv "$PWD/$dotfile" "$HOME"
+
+            # remove old symlink
+            [ -L "$HOME/$dotfile" ] && unlink "$HOME/$dotfile"
+
+            # backup old file
+            [ -e "$HOME/$dotfile" ] && mv -v "$HOME/$dotfile" $BACKUPDIR
+
+            ln -sv "$PWD/$dotfile" "$HOME"
             ;;
     esac
 done
 
+echo "done."
