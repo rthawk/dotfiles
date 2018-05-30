eefe3389a0fc6bdc76acca4cd670dab880a3af82
rthawk <hawk@rtks.net>
2018-05-30 16:40:28 +0900

    remove unused files

--- install.sh
+++ install.sh
@@ -3,6 +3,4 @@
 DOTFILE_PATH="$(dirname "$0")"
 
 cd "$DOTFILE_PATH" || exit 1
-
 ./create_symlink.sh
-./setup_vim_plugins.sh
--- karabiner/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$(uname)" = 'Darwin' ] ; then
-  CONFIG_PATH=~/dotfiles/karabiner/private.xml
-  TARGET_PATH=~/Library/Application\ Support/Karabiner/private.xml
-
-  mv "$TARGET_PATH" "$TARGET_PATH.bk.$(date +%Y-%m-%d.%H%M%S)"
-  ln -sv "$CONFIG_PATH" "$TARGET_PATH"
-fi
