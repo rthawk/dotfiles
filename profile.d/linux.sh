f0270a7095c842fb5a9c1fc03b5f97108bbc4445
rthawk <hawk@rtks.net>
2018-06-02 12:26:10 +0900

    tmux: rename key-table, and sort

--- profile.d/linux.sh
+++ profile.d/linux.sh
@@ -4,10 +4,5 @@ if test "$(uname)" != 'Linux' ;then
   return 0
 fi
 
-# for slimux on tmux 2.2
-export EVENT_NOEPOLL=1
-
 export LS_COLORS='di=33:fi=0:ln=95:pi=5:so=5:bd=5:cd=5:or=37:mi=0:ex=38;05;163:*.rpm=90'
 alias ls="ls --color=auto"
-
-# vim:set filetype=sh:
--- profile.d/osx.sh
+++ profile.d/osx.sh
@@ -4,17 +4,14 @@ if test "$(uname)" != 'Darwin' ;then
   return 0
 fi
 
+stty discard undef # C-o
+
 # homebrew
 export PATH=/usr/local/bin:$PATH
 export PATH="$PATH:/usr/local/sbin"
 
-# for slimux on tmux 2.2
-export EVENT_NOKQUEUE=1
-export EVENT_NOPOLL=1
-
 export LSCOLORS=dxgxcxdxCxegedabagacad
+
 alias ls='ls -G'
-alias man='env LANG=C man'
 alias pwdcp='pwd | pbcopy'
 
-# vim:set filetype=sh:
